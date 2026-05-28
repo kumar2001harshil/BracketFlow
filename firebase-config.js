036f67f870796cc634527a656fb33c081db52426
kumar2001harshil <kumar2001harshil@users.noreply.github.com>
2026-05-28 05:42:42 +0000

    deploy: 9b2ac8dd1b77d1c20f90b642e1ddbfcb99863dbe

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,12 @@
  */
 
 const firebaseConfig = {
-    apiKey: "FIREBASE_API_KEY",
-    authDomain: "FIREBASE_AUTH_DOMAIN",
-    projectId: "FIREBASE_PROJECT_ID",
-    storageBucket: "FIREBASE_STORAGE_BUCKET",
-    messagingSenderId: "FIREBASE_MESSAGING_SENDER_ID",
-    appId: "FIREBASE_APP_ID"
+    apiKey: "AIzaSyBUs0vOViFd28b-V2zVlVKmnx0S1XJmQ2s",
+    authDomain: "bracketflow-58321.firebaseapp.com",
+    projectId: "bracketflow-58321",
+    storageBucket: "bracketflow-58321.firebasestorage.app",
+    messagingSenderId: "1071104966834",
+    appId: "1:1071104966834:web:40a1d0a4a864a8073fc9ab"
 };
 
 // Initialize Firebase
